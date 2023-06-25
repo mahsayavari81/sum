2ac3a4a1e75c65fd47ac1152841aa91272b56cf0
Padidar <Padidar@DESKTOP-NA8IQP4>
2023-06-25 09:20:53 +0330

    Add project files.

---
+++ ConsoleApp6/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+namespace Sum
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Two Numbers:");
+
+            //get inputs from user
+            string input1 = Console.ReadLine();
+            string input2 = Console.ReadLine();
+
+            //convert inputs to int
+            int num1 = Convert.ToInt32(input1);
+            int num2 = Convert.ToInt32(input2);
+
+            //add numbers
+            int sum = num1 + num2;
+
+            //print the sum
+            Console.WriteLine("\nSum is: " + sum);
+        }
+    }
+}
